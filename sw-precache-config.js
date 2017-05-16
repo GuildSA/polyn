8a8d2b4e945834f1b1c38aabcb73facdf282aad6
Kevin Harris <kharris@guildsa.org>
2017-05-15 23:57:04 -0500

    Firebase tweaks.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -8,5 +8,5 @@ module.exports = {
     '/bower_components/webcomponentsjs/*',
   ],
   navigateFallback: 'index.html',
-  navigateFallbackWhitelist: [/^(?!\/__)/, ' /getProjectConfig/'],
+  navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ]
 };
