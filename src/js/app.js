b291626b1e2cb2dd78faf2542457b8dcca3edeb5
Kevin Harris <kharris@guildsa.org>
2017-05-27 00:18:45 -0500

    Remove Backendless test code.

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-
-var APP_ID = '241AA7F2-D35A-9F48-FF5D-856098C84200';
-var API_KEY = 'D63F3664-C3FD-DEEF-FF12-61E65D799100';
-
-Backendless.serverURL = 'https://api.backendless.com';
-Backendless.initApp(APP_ID, API_KEY);
