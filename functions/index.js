5015f3554e24a6bf6e49c055a3d8fd2a63fdc5b3
Kevin Harris <kharris@guildsa.org>
2017-06-07 00:12:04 -0500

    Code cleanup.

--- functions/index.js
+++ functions/index.js
@@ -11,51 +11,51 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
+// exports.helloWorld = functions.https.onRequest((req, res) => {
 
-  cors(req, res, () => {
-     res.send("Hello from Firebase!");
-  });
-});
+//   cors(req, res, () => {
+//      res.send("Hello from Firebase!");
+//   });
+// });
 
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
+// // Take the text parameter passed to this HTTP endpoint and insert it into the
+// // Realtime Database under the path /messages/:pushId/original
+// exports.addMessage = functions.https.onRequest((req, res) => {
 
-  // Grab the 'text' parameter.
-  const original = req.query.text;
+//   // Grab the 'text' parameter.
+//   const original = req.query.text;
 
-  // Push it into the Realtime Database then send a response
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+//   // Push it into the Realtime Database then send a response
+//   admin.database().ref('/messages').push({original: original}).then(snapshot => {
 
-// NOTE: The use of cors is needed here but according to the spec, it is not 
-// supposedly needed for GETs or POSTs. I'm not sure why we need it for an iron-ajax POST.
-    cors(req, res, () => {
-      //res.redirect(303, snapshot.ref); // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-      //res.send("SUCCESS");
-      res.status(200).end();
-    });
+// // NOTE: The use of cors is needed here but according to the spec, it is not 
+// // supposedly needed for GETs or POSTs. I'm not sure why we need it for an iron-ajax POST.
+//     cors(req, res, () => {
+//       //res.redirect(303, snapshot.ref); // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//       //res.send("SUCCESS");
+//       res.status(200).end();
+//     });
 
-  });
-});
+//   });
+// });
 
-// If we add an onWrite listener to /messages/:pushId/original, we can create an
-// uppercase version of the message and save it to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onWrite(event => {
+// // If we add an onWrite listener to /messages/:pushId/original, we can create an
+// // uppercase version of the message and save it to /messages/:pushId/uppercase
+// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onWrite(event => {
 
-    // Grab the current value of what was written to the Realtime Database.
-    const original = event.data.val();
-    console.log('Uppercasing', event.params.pushId, original);
-    const uppercase = original.toUpperCase();
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    return event.data.ref.parent.child('uppercase').set(uppercase);
-});
+//     // Grab the current value of what was written to the Realtime Database.
+//     const original = event.data.val();
+//     console.log('Uppercasing', event.params.pushId, original);
+//     const uppercase = original.toUpperCase();
+//     // You must return a Promise when performing asynchronous tasks inside a Functions such as
+//     // writing to the Firebase Realtime Database.
+//     // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+//     return event.data.ref.parent.child('uppercase').set(uppercase);
+// });
 
 exports.sendMessage = functions.https.onRequest((req, res) => {
 
@@ -103,7 +103,7 @@ exports.sendRequest = functions.https.onRequest((req, res) => {
       timeStamp: admin.database.ServerValue.TIMESTAMP
   };
 
-  // Push it into the Realtime Database then send a response
+  // Add the request under the given category.
   admin.database().ref('/requests/' + categoryPath + '/requests').push(request).then(snapshot => {
 
     // Get all the sellers for this category.
@@ -158,15 +158,10 @@ exports.sendRequest = functions.https.onRequest((req, res) => {
                     console.log("Error sending message:", error);
                     res.status(500).end();
                   });
-
                 });
-
               }
-
             });
-
         });
-
     });
 
     cors(req, res, () => {
@@ -176,12 +171,3 @@ exports.sendRequest = functions.https.onRequest((req, res) => {
     });
   });
 });
-
-// How to test:
-// Function URL (helloWorld): https://us-central1-polyn-3c431.cloudfunctions.net/helloWorld
-// Function URL (addMessage): https://us-central1-polyn-3c431.cloudfunctions.net/addMessage
-
-// Notes:
-// https://github.com/firebase/functions-samples
-// https://codelabs.developers.google.com/codelabs/firebase-cloud-functions/#9
-
