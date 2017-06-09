99b945e32e03ad1accc70f8755abc6499331fd7a
Kevin Harris <kharris@guildsa.org>
2017-06-09 18:27:01 -0500

    Evil code to fix mobile Safari’s evil image orientation issue!

--- src/js/Utility.js
+++ src/js/Utility.js
@@ -29,6 +29,101 @@ var Utility = (function() {
     _errorToast.open();
   };
 
+  // https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side
+  // -2: not jpeg
+  // -1: not defined
+  var getOrientation = function(file, callback) {
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+
+      var view = new DataView(e.target.result);
+      if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+      var length = view.byteLength, offset = 2;
+      while (offset < length) {
+        var marker = view.getUint16(offset, false);
+        offset += 2;
+        if (marker == 0xFFE1) {
+          if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+          for (var i = 0; i < tags; i++)
+            if (view.getUint16(offset + (i * 12), little) == 0x0112)
+              return callback(view.getUint16(offset + (i * 12) + 8, little));
+        }
+        else if ((marker & 0xFF00) != 0xFF00) break;
+        else offset += view.getUint16(offset, false);
+      }
+      return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  var getBase64 = function(file, callback) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      callback(reader.result);
+    };
+    reader.onerror = function (error) {
+//console.log('Error: ', error);
+      callback('Error: ', error);
+    };
+  }
+
+  // https://stackoverflow.com/questions/20600800/js-client-side-exif-orientation-rotate-and-mirror-jpeg-images
+  var resetOrientation = function(srcBase64, srcOrientation, callback) {
+    var img = new Image();    
+
+    img.onload = function() {
+      var width = img.width,
+          height = img.height,
+          canvas = document.createElement('canvas'),
+          ctx = canvas.getContext("2d");
+
+      // set proper canvas dimensions before transform & export
+      if ([5,6,7,8].indexOf(srcOrientation) > -1) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      }
+
+      // transform context before drawing image
+      switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+        case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+        case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+        case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+        default: ctx.transform(1, 0, 0, 1, 0, 0);
+      }
+
+      // draw image
+      ctx.drawImage(img, 0, 0);
+
+      // export base64
+      callback(canvas.toDataURL());
+    };
+
+    img.src = srcBase64;
+  };
+
+  // https://stackoverflow.com/questions/23150333/html5-javascript-dataurl-to-blob-blob-to-dataurl
+  var dataURLtoBlob = function(dataurl) {
+      var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+          bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+      while(n--){
+          u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new Blob([u8arr], {type:mime});
+  }
+
   function log(message) {
     console.log("Utility: " + message);
   }
@@ -37,6 +132,10 @@ var Utility = (function() {
   // our private functions.
   return {
     showErrorToast: showErrorToast,
+    getOrientation: getOrientation,
+    resetOrientation: resetOrientation,
+    getBase64: getBase64,
+    dataURLtoBlob: dataURLtoBlob
   }
 
 })();
